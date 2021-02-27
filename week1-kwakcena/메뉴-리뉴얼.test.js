2592e36b03a642d1add24120b7bbe45993367090
Kwakcena <rhkrgudwh@gmail.com>
2021-02-27 17:13:48 +0900

    조합 결과에 대한 객체를 만든다
    조합 결과를 하나의 배열로 모아 이에 대한 key-value쌍으로 만들어 몇개가
    있는지 카운팅 한다.

--- week1-kwakcena/메뉴-리뉴얼.test.js
+++ week1-kwakcena/메뉴-리뉴얼.test.js
@@ -1,3 +1,5 @@
+const { test, expect } = require('@jest/globals');
+
 const solution = (n) => {
   return 1;
 };
@@ -19,6 +21,13 @@ const combination = (order, n) => {
 
   go([], 0);
   return result;
+};
+
+const countOrders = (orders) => {
+  return orders.reduce(
+    (acc, order) => ({ ...acc, [order]: (acc[order] || 0) + 1 }),
+    {}
+  );
 };
 
 test('solution', () => {
@@ -54,4 +63,32 @@ test('각 손님별로 나올 수 있는 n개의 조합을 구한다', () => {
     'DH',
     'EH',
   ]);
+});
+
+test('n개의 조합 결과를 카운팅 하기 위해 key-value의 객체로 만든다', () => {
+  const orders = ['ABCFG', 'AC', 'CDE', 'ACDE', 'BCFG', 'ACDEH'];
+  const result = orders
+    .map((order) => combination(order, 2))
+    .reduce((acc, it) => [...acc, ...it], []);
+  expect(countOrders(result)).toEqual({
+    AB: 1,
+    AC: 4,
+    AD: 2,
+    AE: 2,
+    AF: 1,
+    AG: 1,
+    AH: 1,
+    BC: 2,
+    BF: 2,
+    BG: 2,
+    CD: 3,
+    CE: 3,
+    CF: 2,
+    CG: 2,
+    CH: 1,
+    DE: 3,
+    DH: 1,
+    EH: 1,
+    FG: 2,
+  });
 });
