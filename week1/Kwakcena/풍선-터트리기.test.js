21e83003cdb8e2d154bc1e78ade8a27b42e846ee
Kwakcena <rhkrgudwh@gmail.com>
2021-03-05 23:59:55 +0900

    Fail] 런타임 에러 발생
    1, 2, 3, 4번 문제를 제외하고 런타임 에러가 발생한다.
    
    Math.min의 함수에서 런타임 에러가 나는걸로 예상된다. 직접 for문을 돌려
    작은값을 찾으면 바로 탈출하도록 바꿔봐야겠다.

---
+++ week1/Kwakcena/풍선-터트리기.test.js
@@ -0,0 +1,35 @@
+const solution = (a) => {
+  // const answer = [];
+  // for (let i = 0; i < a.length; i++) {
+  //   const left = isExistMinValue(a[i], a.slice(0, i));
+  //   const right = isExistMinValue(a[i], a.slice(i + 1, a.length));
+  //   if (left && right) continue;
+  //   answer.push(a[i]);
+  // }
+  // return answer.length;
+  return a.filter((value, index) =>
+    [
+      isExistMinValue(value, a.slice(0, index)),
+      isExistMinValue(value, a.slice(index + 1, a.length)),
+    ].some((x) => !x)
+  ).length;
+};
+
+const isExistMinValue = (value, array) => value > Math.min(...array);
+
+test('solution', () => {
+  expect(solution([-16, 27, 65, -2, 58, -92, -71, -68, -61, -33])).toBe(6);
+  expect(solution([9, -1, 5])).toBe(3);
+});
+
+test('자신보다 작은 값이 있는지 찾는다', () => {
+  expect(isExistMinValue(-16, [27, 65, -2, 58, -92, -71, -68, -61, -33])).toBe(
+    true
+  );
+  expect(isExistMinValue(27, [-16])).toBe(true);
+  expect(isExistMinValue(27, [65, -2, 58, -92, -71, -68, -61, -33])).toBe(true);
+  expect(isExistMinValue(65, [-16, 27])).toBe(true);
+  expect(isExistMinValue(65, [-2, 58, -92, -71, -68, -61, -33])).toBe(true);
+  expect(isExistMinValue(-92, [-16, 27, 65, -2, 58])).toBe(false);
+  expect(isExistMinValue(-92, [-71, -68, -61, -33])).toBe(false);
+});
