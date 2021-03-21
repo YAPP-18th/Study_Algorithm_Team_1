d34c21e4a00d17e272c6fd574c5d80838b0474e7
Kwakcena <rhkrgudwh@gmail.com>
2021-03-21 23:25:41 +0900

    수식 최대화 문제 풀이
    수식 최대화 문제를 풀었습니다.

--- week3/maximizeExpression.test.js
+++ week3/maximizeExpression.test.js
@@ -1,7 +1,104 @@
 const solution = (expression) => {
-  return 60420;
+  const operators = combination(getOperator(expression));
+  const array = toArray(expression);
+  return Math.max(...operators.map((oper) => calculate(oper, [...array])));
+};
+
+const getOperator = (expression) => [
+  ...new Set(expression.replace(/[0-9]/g, '').split('')),
+];
+
+const combination = (operators) => {
+  const result = [];
+  const check = Array(operators.length).fill(false);
+  const go = (target, index) => {
+    if (index === operators.length) {
+      result.push(target);
+      return;
+    }
+    for (let i = 0; i < operators.length; i++) {
+      if (check[i]) continue;
+      target.push(operators[i]);
+      check[i] = true;
+      go([...target], index + 1);
+      check[i] = false;
+      target.pop();
+    }
+  };
+
+  go([], 0);
+  return result;
+};
+
+const toArray = (expression) => {
+  return expression
+    .replace(/(\*|\-|\+)(?!$)/g, ' $1 ')
+    .split(' ')
+    .map((x) => (/\+|\-|\*/.test(x) ? x : x | 0));
+};
+
+const calculate = (operators, expression) => {
+  const cal = {
+    '+': (x, y) => x + y,
+    '-': (x, y) => x - y,
+    '*': (x, y) => x * y,
+  };
+
+  for (let i = 0; i < operators.length; i++) {
+    const oper = operators[i];
+    while (expression.includes(oper)) {
+      const index = expression.findIndex((it) => it === oper);
+      const number = cal[oper](expression[index - 1], expression[index + 1]);
+      expression.splice(index - 1, 3, number);
+    }
+  }
+  return expression[0] < 0 ? -expression[0] : expression[0];
 };
 
 test('solution', () => {
   expect(solution('100-200*300-500+20')).toBe(60420);
+});
+
+test('연산자를 뽑아낸다.', () => {
+  expect(getOperator('100-200*300-500+20')).toEqual(['-', '*', '+']);
+});
+
+test('수식을 배열의 원소로 뽑아낸다', () => {
+  expect(toArray('100-200*300-500+20')).toEqual([
+    100,
+    '-',
+    200,
+    '*',
+    300,
+    '-',
+    500,
+    '+',
+    20,
+  ]);
+});
+
+test('뽑아낸 연산자로 우선순위에 따른 조합을 구한다', () => {
+  expect(combination(['-', '*', '+'])).toEqual([
+    ['-', '*', '+'],
+    ['-', '+', '*'],
+    ['*', '-', '+'],
+    ['*', '+', '-'],
+    ['+', '-', '*'],
+    ['+', '*', '-'],
+  ]);
+});
+
+test('뽑아낸 숫자를 연산자 우선순위 배열에 따라 연산한 결과를 구한다', () => {
+  expect(
+    calculate(['*', '+', '-'], [100, '-', 200, '*', 300, '-', 500, '+', 20])
+  ).toBe(60420);
+  expect(
+    calculate(['-', '*', '+'], [100, '-', 200, '*', 300, '-', 500, '+', 20])
+  ).toBe(20020);
+  expect(
+    calculate(['-', '+', '*'], [100, '-', 200, '*', 300, '-', 500, '+', 20])
+  ).toBe(18000);
+  expect(
+    calculate(['*', '-', '+'], [100, '-', 200, '*', 300, '-', 500, '+', 20])
+  ).toBe(60380);
 });
